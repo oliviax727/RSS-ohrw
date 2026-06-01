606fa1eb1e5f17134c14c1b0cdcc5ff102d8f550
Oliviax727 <oliviahrwalters@gmail.com>
2026-06-01 20:18:35 +0800

    moved rss applet to seperate html

--- src/scripts/propagate-basic-changes.sh
+++ src/scripts/propagate-basic-changes.sh
@@ -1,1 +1,10 @@
-#!/bin/bash
+html_files=(src/html/*.html)
+
+local_html_file=$(cat src/layout/content-template.html)
+
+for html in "${html_files[@]}"
+do
+    if [ "$html" != "src/html/primary.html" ]; then
+        echo "$local_html_file" > $html
+    fi
+done
