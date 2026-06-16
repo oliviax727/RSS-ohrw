c4be07b19513bb61212af5b24a248cb3c8467608
Oliviax727 <oliviahrwalters@gmail.com>
2026-06-16 15:24:06 +0800

    adblocker test

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ const DEFAULT_SECTION = 'primary';
 
 let mhtml = new MainHTML(SECTION_COLOR_DICT, CRUNCH_SIZE, DEFAULT_SECTION);
 
-window.bm = BoneMiner;
 window.help = Helpers;
+window.bm = BoneMiner;
 
 window.onload = mhtml.initPage;
 window.onresize = mhtml.crunch;
