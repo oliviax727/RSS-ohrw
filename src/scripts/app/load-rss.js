31779216d745b5e759cd731dec867d1fa5406214
Oliviax727 <oliviahrwalters@gmail.com>
2026-06-16 22:17:36 +0800

    compile ts to webjs

---
+++ src/scripts/app/load-rss.js
@@ -0,0 +1,95 @@
+/// <reference types="node" />
+import Parser, {} from 'https://cdn.jsdelivr.net/npm/rss-parser';
+export var LoadRSS;
+(function (LoadRSS) {
+    // Load RSS object from XML
+    let ObjectXML;
+    (function (ObjectXML) {
+        // RSS Feed
+        class Feed {
+            urlList;
+            entryList = new Array();
+            constructor(urlList) {
+                this.urlList = urlList;
+            }
+            // Generate the collection of items based on the feed
+            async createFeed() {
+                const promisedEntries = Array.from(this.urlList).map(async ([name, url]) => {
+                    console.log("Getting data from: " + url);
+                    let feedData = await RSSHelper.getXML(url);
+                    const entriesToAdd = this.parsedXMLToEntries(feedData, name);
+                    return entriesToAdd;
+                });
+                const unconcatenatedEntries = await Promise.all(promisedEntries);
+                unconcatenatedEntries.forEach((entrySet) => {
+                    this.entryList = [...this.entryList, ...entrySet];
+                });
+                this.sortFeed();
+            }
+            // Sort feed array based on date
+            sortFeed() {
+                this.entryList.sort((a, b) => {
+                    if (a.date !== undefined && b.date !== undefined) {
+                        return (+b.date) - (+a.date);
+                    }
+                    else {
+                        return b.uuid - a.uuid;
+                    }
+                });
+            }
+            // Parsed XML data to entry
+            parsedXMLToEntries(xmlData, feedName) {
+                const channelData = this.channelToParentData(xmlData, feedName);
+                let entries = new Set(xmlData.items.map((item) => {
+                    return this.itemToEntry(item, channelData);
+                }));
+                return entries;
+            }
+            // Load parent channel data into ParentData object
+            channelToParentData(xmlData, feedName) {
+                return {
+                    uuid: RSSHelper.uuidURL(xmlData.link),
+                    name: feedName,
+                    title: xmlData.title,
+                    link: xmlData.link,
+                    imageName: xmlData.image?.title,
+                    imageUrl: xmlData.image?.url
+                };
+            }
+            itemToEntry(xmlItem, itemParent) {
+                return {
+                    uuid: RSSHelper.uuidURL(xmlItem.link),
+                    link: xmlItem.link,
+                    title: xmlItem.title,
+                    description: xmlItem.contentSnippet,
+                    date: typeof xmlItem.pubDate === "string" ? new Date(xmlItem.pubDate) : undefined,
+                    parentData: itemParent
+                };
+            }
+        }
+        ObjectXML.Feed = Feed;
+    })(ObjectXML = LoadRSS.ObjectXML || (LoadRSS.ObjectXML = {}));
+    // Helper functions for the LoadRSS
+    let RSSHelper;
+    (function (RSSHelper) {
+        const rssParser = new Parser();
+        function uuidURL(url) {
+            function getHash(str, seed = 5381) {
+                for (let i = 0; i < str.length; i++) {
+                    // hash * 33 + charCode (bitwise shift for efficiency: hash << 5 is hash * 32)
+                    seed = (seed << 5) + seed + str.charCodeAt(i);
+                }
+                // Convert to unsigned 32-bit integer to avoid negative values
+                return seed >>> 0;
+            }
+            return getHash(url);
+        }
+        RSSHelper.uuidURL = uuidURL;
+        // Retreive XML file
+        async function getXML(url) {
+            let feed = await rssParser.parseURL(url);
+            return feed;
+        }
+        RSSHelper.getXML = getXML;
+    })(RSSHelper = LoadRSS.RSSHelper || (LoadRSS.RSSHelper = {}));
+})(LoadRSS || (LoadRSS = {}));
--- src/scripts/compile-ts-to-webjs.sh
+++ src/scripts/compile-ts-to-webjs.sh
@@ -1,3 +1,22 @@
-# Compile to js folder
-tsc
+(
+    # Compile to js folder
+    tsc
 
+    # Compile app folder
+    cp -r ./src/scripts/js/* ./src/scripts/app/
+
+    js_files=(./src/scripts/app/*)
+    line_regex='^(?!(.*(\/\/)))(import\h+(\S+\h+)+from\s+)('\''\S+'\'')(\h|;)*$'
+    modl_regex='(?<=from\s'\'')(\w|[\-\_\.])+(?='\''|\h|;)'
+    cdn_http='https://cdn.jsdelivr.net/npm/'
+
+    for js_file in "${js_files[@]}"; do
+        line="$(ggrep -oP "$line_regex" $js_file)"
+        item="$(echo "$line" | ggrep -oP "$modl_regex")"
+        new_line="${line/$item/$cdn_http$item}"
+        if [[ -n "$line" ]]; then
+            echo "$new_line"
+            gsed -i "s|$line|$new_line|" $js_file
+        fi
+    done
+)
