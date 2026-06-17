7b75f1d919470a182271c7359eeb303ba3e490a7
Oliviax727 <oliviahrwalters@gmail.com>
2026-06-17 15:47:23 +0800

    other refactoring and reorganising changes

--- src/scripts/cmd/compile-app.sh
+++ src/scripts/cmd/compile-app.sh
@@ -3,8 +3,6 @@
     tsc
 
     # Compile app folder
-    cp -r ./src/scripts/js/* ./src/scripts/app/
-
     js_files=(./src/scripts/app/*)
     line_regex='^(?!(.*(\/\/)))(import\h+(\S+\h+)+from\s+)('\''\S+'\'')(\h|;)*$'
     modl_regex='(?<=from\s'\'')(\w|[\-\_\.])+(?='\''|\h|;)'
@@ -15,7 +13,6 @@
         item="$(echo "$line" | ggrep -oP "$modl_regex")"
         new_line="${line/$item/$cdn_http$item}"
         if [[ -n "$line" ]]; then
-            echo "$new_line"
             gsed -i "s|$line|$new_line|" $js_file
         fi
     done
